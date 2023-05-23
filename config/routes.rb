eb894f6c0e1df1337cf60c3cf813abb20edaa3be
Ben Pham <benjamin.pham@gmail.com>
2023-05-23 10:15:44 +0200

    Restaurant index, show, new, create

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,14 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # Index
+  get '/restaurants', to: 'restaurants#index'
+
+  # Create
+  get '/restaurants/new', to: 'restaurants#new'
+  post '/restaurants', to: 'restaurants#create'
+
+  # Show
+  get '/restaurants/:id', to: 'restaurants#show', as: 'restaurant'
 end
